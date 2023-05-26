1af3418eab0e392dbb1b411f812ca89bfaefa1fc
madihacalico <133571089+madihacalico@users.noreply.github.com>
2023-05-26 22:53:03 +0800

    Add files via upload

---
+++ Q8mazeSolver/Coordinate.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Q8mazeSolver;
+
+public class Coordinate {
+    int x;
+    int y;
+    Coordinate parent;
+
+    public Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.parent = null;
+    }
+
+    public Coordinate(int x, int y, Coordinate parent) {
+        this.x = x;
+        this.y = y;
+        this.parent = parent;
+    }
+
+    int getX() {
+        return x;
+    }
+
+    int getY() {
+        return y;
+    }
+
+    Coordinate getParent() {
+        return parent;
+    }
+}
---
+++ Q8mazeSolver/MazeDriver.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Q8mazeSolver;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.List;
+import java.util.Scanner;
+
+public class MazeDriver {
+    public static void main(String[] args) throws Exception {
+        File maze1 = new File("C:\\Users\\user\\Downloads\\maze1.txt");
+        
+        print(maze1);
+        execute(maze1);
+    }
+
+    private static void execute(File file) throws Exception {
+        Maze maze = new Maze(file);
+        //dfs(maze);
+        bfs(maze);
+    }
+    
+    private static void print(File file) throws Exception {
+        System.out.println("Hua Rong Road: ");
+        file = new File("C:\\Users\\user\\Downloads\\maze1.txt");
+        Scanner sc = new Scanner(file);
+        while (sc.hasNextLine()){
+            System.out.println(sc.nextLine());
+        }
+        System.out.println("");
+    }
+
+    private static void bfs(Maze maze) {
+        System.out.println("How Cao Cao might have retreated from Hua Rong Road: ");
+        BFSMazeSolver bfs = new BFSMazeSolver();
+        List<Coordinate> path = bfs.solve(maze);
+        maze.printPath(path);
+        maze.reset();
+    }
+
+    private static void dfs(Maze maze) {
+        DFSMazeSolver dfs = new DFSMazeSolver();
+        List<Coordinate> path = dfs.solve(maze);
+        maze.printPath(path);
+        maze.reset();
+    }
+}
